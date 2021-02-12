3320b1645a3cdc99e2d8011f2ba99258cae256a2
Zyter <60459687+remter@users.noreply.github.com>
2021-02-12 04:27:57 -0800

    Updated guided project
    Downloaded files from the end of the guided project.

--- Twitter_clone/after the coursera guided project/twitter-clone/pages/index.js
+++ Twitter_clone/after the coursera guided project/twitter-clone/pages/index.js
@@ -1,65 +1,29 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import Tweet from "./Tweet"
 
-export default function Home() {
+import path from 'path'
+import fs from 'fs'
+
+export default function Home({tweets}) {
   return (
     <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+      <h1>Hello, World!</h1>
+      
+      {tweets.map(tweet => <Tweet key = {tweet.id} text = {tweet.text} author= {tweet.author} date = {tweet.date} />)}
     </div>
   )
+}
+
+export async function getServerSideProps(){
+  const filePath1 = path.join(process.cwd(), 'data.json')
+  const tweets1 = JSON.parse(fs.readFileSync(filePath1))
+  const filePath2 = path.join(process.cwd(), 'data2.json')
+  const tweets2 = JSON.parse(fs.readFileSync(filePath2))
+  const tweets = tweets1.concat(tweets2)
+  return{
+    props: {
+      tweets
+    }
+  }
 }
